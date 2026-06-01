8a152e61da01342eb9451a74f6f2ad64dd6862c3
daudikombe <daudikombe77@gmail.com>
2026-06-01 17:15:13 +0300

    a function that identify most common word

--- pythonAssesment.py
+++ pythonAssesment.py
@@ -9,3 +9,22 @@ def count_specific_word(text: str, word: str) -> int:
     words = re.findall(r'\w+', text_lower)
     count = words.count(word_lower)
     return count
+
+    def identify_most_common_word(text: str) ->str:
+        if not text or text.strip() == "":
+            return None
+        words = re.findall(r'\w+', text.lower())
+        if not words:
+            return None
+
+            frequency = {}
+            for word in words:
+                frequency[word] = frequency.get(word, 0) + 1
+
+                most_common_word = words[0]
+                max_count = frequency[most_common_word]
+                for word in words:
+                    if frequency[word] > max_count:
+                        most_common_word = word
+                        max_count = frequency[word]
+                return most_common_word    
